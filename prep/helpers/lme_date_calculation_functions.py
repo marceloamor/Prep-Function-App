9732de5467a45bd95e1dbf052a3dec577376084f
fwf-upetrading <frederick.fillingham@upetrading.com>
2023-07-27 17:02:35 +0100

    Add basic logic for handling other prompts and friday nonprompts

--- prep/helpers/lme_date_calculation_functions.py
+++ prep/helpers/lme_date_calculation_functions.py
@@ -14,12 +14,28 @@ def get_lme_non_prompt_map(non_prompts: List[date]) -> Dict[date, date]:
         day = non_prompt.day
         month = non_prompt.month
         weekday = non_prompt.weekday
+        is_rollback_friday_nonprompt = False
         if non_prompt == easter_friday_date:
             # easter friday prompts fall on the prior day
             non_prompt_map[non_prompt] = non_prompt - offset_1d
+            is_rollback_friday_nonprompt = True
         elif month == 12 and day == 25 and weekday in (1, 2, 3, 4):
             # christmas day if it's a tuesday through friday is the prior settlement day
             non_prompt_map[non_prompt] = non_prompt - offset_1d
+            is_rollback_friday_nonprompt = True
+        else:
+            # all other non-prompts roll forwards to the next succeeding day which is a
+            # settlement business day rule (8.4.1)
+            next_valid_settlement_day_guess = non_prompt + offset_1d
+            # while next_valid_settlement_day_guess in
+            non_prompt_map[non_prompt] = non_prompt
+
+        if is_rollback_friday_nonprompt:
+            # specific fridays will roll back but the subsequent saturday needs to be
+            # mapped to the following monday
+            non_prompt_map[
+                non_prompt + offset_1d
+            ] = non_prompt + relativedelta.relativedelta(days=3)
         # finish writing cases make sure that they don't overlap or end up in endless loops
         # probably requires a verification step
 
