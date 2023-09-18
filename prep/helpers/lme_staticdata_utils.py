4925d969438e885fdabbfa16ef8c4048134f6aa6
fwf-upetrading <frederick.fillingham@upetrading.com>
2023-09-18 16:48:08 +0100

    Add `gen_lme_options`

--- prep/helpers/lme_staticdata_utils.py
+++ prep/helpers/lme_staticdata_utils.py
@@ -1,9 +1,20 @@
-from upedata.static_data import Product, Future, PriceFeed, FuturePriceFeedAssociation
-from prep.helpers.date_name_utilities import MONTH_CODE_MAPPING
-from prep.exceptions import ProductNotFound
+from upedata.static_data import (
+    Product,
+    Future,
+    PriceFeed,
+    FuturePriceFeedAssociation,
+    Option,
+)
 
-from datetime import datetime, date
-from typing import List
+from upedata.template_language import parser
+from upedata.dynamic_data import VolSurface
+from prep.exceptions import ProductNotFound
+import upedata.enums as upe_enums
+
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from typing import List, Dict
 
 
 LME_FUTURE_MULTIPLIERS = {"lad": 25, "lcu": 25, "pbd": 25, "lzh": 25, "lnd": 6}
@@ -16,10 +27,8 @@ LME_FUTURE_3M_FEED_ASSOC = {
 }
 
 
-def gen_lme_futures(
-    expiry_dates: List[datetime], product: Product, generate_options=False
-) -> List[Future]:
-    static_data_futures = []
+def gen_lme_futures(expiry_dates: List[datetime], product: Product) -> List[Future]:
+    static_data_futures: List[Future] = []
     product_3m_feed = PriceFeed(
         feed_id=LME_FUTURE_3M_FEED_ASSOC[product.short_name],
         origin="cqg",
@@ -57,8 +66,6 @@ def gen_lme_futures(
                 product_3m_future_price_feed_assoc,
                 product_relative_spread_feed,
             ]
-            if generate_options:
-                pass
 
         except KeyError:
             raise ProductNotFound(
@@ -67,3 +74,48 @@ def gen_lme_futures(
         static_data_futures.append(new_lme_future)
 
     return static_data_futures
+
+
+def gen_lme_options(
+    futures_list: List[Future], option_specification_data: Dict
+) -> List[Option]:
+    two_week_td = relativedelta(days=14)
+    generated_options: List[Option] = []
+    for future in futures_list:
+        # first need to check to make sure it's on a third Wednesday, as these will
+        # always be underlying of an LME option.
+        # if not just ignore
+        if future.expiry.weekday() == 2:
+            if 15 <= future.expiry.day and future.expiry.day <= 21:
+                # the timezone fuckery is in case of something *somehow* crossing a dst
+                # boundary, odds are this won't happen but you never know.
+                option_expiry_date = (
+                    future.expiry.replace(tzinfo=ZoneInfo("Europe/London"))
+                    - two_week_td
+                ).replace(hour=11, minute=15)
+                general_option_data = (
+                    option_specification_data["specific"][future.product.symbol.lower()]
+                    | option_specification_data["shared"]
+                )
+                generated_option = Option(
+                    symbol=f"{future.product.symbol} o {option_expiry_date.strftime(r'%y-%m-%d')} a",
+                    multiplier=general_option_data["multiplier"],
+                    strike_intervals=general_option_data["strike_intervals"],
+                    expiry=option_expiry_date,
+                    display_name=option_specification_data["shared"]["display_name"],
+                    product=future.product,
+                    underlying_future=future,
+                    vol_surface=VolSurface(
+                        model_type=general_option_data["vol_surface"]["model_type"],
+                        expiry=option_expiry_date,
+                        params=general_option_data["vol_surface"]["params"],
+                    ),
+                    vol_type=upe_enums.VolType(general_option_data["vol_type"]),
+                    time_type=upe_enums.TimeType(general_option_data["time_type"]),
+                )
+                generated_option = parser.substitute_derivative_generation_time(
+                    generated_option
+                )
+                generated_options.append(generated_option)
+
+    return generated_options
--- tests/helpers/test_lme_staticdata_utils.py
+++ tests/helpers/test_lme_staticdata_utils.py
@@ -1,10 +1,13 @@
 from prep.helpers import lme_staticdata_utils, date_name_utilities
-from upedata.static_data import Product, Future
+from upedata.static_data import Product, Future, Option
+from upedata.dynamic_data import VolSurface
+from upedata import enums as upe_enums
 
 import pytest
 
 from datetime import datetime, date
-from typing import List
+from typing import List, Dict
+from zoneinfo import ZoneInfo
 import logging
 
 
@@ -68,3 +71,105 @@ def test_generate_staticdata_lme_futures_from_prompts(
     for sd_future, expected_d_name in zip(staticdata_futures, expected_display_names):
         assert sd_future.display_name == expected_d_name, "Display name mismatch"
         assert sd_future.multiplier == expected_multiplier, "Multiplier mismatch"
+
+
+@pytest.mark.parametrize(
+    ["input_futures_list", "option_template_data", "expected_options_list"],
+    [
+        [
+            [
+                Future(
+                    symbol="xlme-lad-usd f 23-11-15",
+                    display_name="LAD 2023-11-15",
+                    product=Product(symbol="xlme-lad-usd", short_name="lad"),
+                    expiry=datetime(2023, 11, 15, 19, 30),
+                ),
+                Future(
+                    symbol="xlme-lad-usd f 23-11-14",
+                    display_name="LAD 2023-11-14",
+                    product=Product(symbol="xlme-lad-usd", short_name="lad"),
+                    expiry=datetime(2023, 11, 14, 19, 30),
+                ),
+            ],
+            {
+                "shared": {
+                    "time_type": 1,
+                    "vol_type": 1,
+                    "display_name": "#{<option>.product.short_name}£o#{map_month_year_coded(<option>.underlying_future.expiry)}£ @{strike}£ @{call_or_put}£",
+                },
+                "specific": {
+                    "xlme-lad-usd": {
+                        "vol_surface": {
+                            "model_type": "delta_spline_wing",
+                            "params": {
+                                "50 Delta": 0.2,
+                                "+25 DIFF": 0.01,
+                                "-25 DIFF": 0.01,
+                                "+10 DIFF": 0.02,
+                                "-10 DIFF": 0.02,
+                            },
+                        },
+                        "strike_intervals": [[1200, 25], [5000, -1]],
+                        "multiplier": 25,
+                    }
+                },
+            },
+            [
+                Option(
+                    display_name=r"ladox3 @{strike}£ @{call_or_put}£",
+                    symbol="xlme-lad-usd o 23-11-01 a",
+                    multiplier=25,
+                    strike_intervals=[[1200, 25], [5000, -1]],
+                    expiry=datetime(
+                        2023, 11, 1, 11, 15, tzinfo=ZoneInfo("Europe/London")
+                    ),
+                    vol_surface=VolSurface(
+                        model_type="delta_spline_wing",
+                        expiry=datetime(
+                            2023, 11, 1, 11, 15, tzinfo=ZoneInfo("Europe/London")
+                        ),
+                        params={
+                            "50 Delta": 0.2,
+                            "+25 DIFF": 0.01,
+                            "-25 DIFF": 0.01,
+                            "+10 DIFF": 0.02,
+                            "-10 DIFF": 0.02,
+                        },
+                    ),
+                    vol_type=upe_enums.VolType.STANDARD,
+                    time_type=upe_enums.TimeType.FULL_YEAR,
+                )
+            ],
+        ]
+    ],
+)
+def test_generate_staticdata_lme_options_from_futures(
+    input_futures_list: List[Future],
+    option_template_data: Dict,
+    expected_options_list: List[Option],
+):
+    options_list = lme_staticdata_utils.gen_lme_options(
+        input_futures_list, option_template_data
+    )
+
+    assert len(options_list) == len(
+        expected_options_list
+    ), "Unexpected number of options generated"
+    for gen_option, expec_option in zip(options_list, expected_options_list):
+        logging.warning(gen_option.symbol)
+        assert (
+            gen_option.display_name == expec_option.display_name
+        ), "Display name mismatch"
+        assert gen_option.symbol == expec_option.symbol, "Symbol mismatch"
+        assert gen_option.multiplier == expec_option.multiplier, "Multiplier mismatch"
+        assert (
+            gen_option.strike_intervals == expec_option.strike_intervals
+        ), "Strike intervals mismatch"
+        assert (
+            gen_option.vol_surface.params == expec_option.vol_surface.params
+        ), "Vol surface params mismatch"
+        assert (
+            gen_option.vol_surface.model_type == expec_option.vol_surface.model_type
+        ), "Vol surface model type mismatch"
+        assert gen_option.time_type == expec_option.time_type, "Time type mistmatch"
+        assert gen_option.vol_type == expec_option.vol_type, "Vol type mismatch"
