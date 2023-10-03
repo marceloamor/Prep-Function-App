dbaf3f24b3824a702b922668a187858d50dc68ee
fwf-upetrading <frederick.fillingham@upetrading.com>
2023-10-03 14:40:49 +0100

    Add stub methods for lme product static data updates and overnight files

--- prep/function_app.py
+++ prep/function_app.py
@@ -1,6 +1,11 @@
+from prep.helpers import lme_staticdata_utils
+
+from upedata.static_data import Exchange
+
 from redis.backoff import ExponentialBackoff
 import azure.functions as func
 from redis.retry import Retry
+import sqlalchemy.orm
 import sqlalchemy
 import redis
 
@@ -32,4 +37,21 @@ sqlalchemy_pg_url = sqlalchemy.URL(
 )
 
 pg_engine = sqlalchemy.create_engine(sqlalchemy_pg_url, echo=False)
-sessionmaker = sqlalchemy.orm.sessionmaker(pg_engine, expire_on_commit=False)
+
+
+@app.function_name(name="rjo_sftp_lme_overnight_poll")
+# @app.schedule(schedule=)
+def check_for_new_lme_overnight_files(timer: func.TimerRequest):
+    pass
+
+
+@app.function_name(name="lme_date_data_updater")
+@app.schedule(schedule="32 1 * * MON-FRI", arg_name="timer", run_on_startup=True)
+def update_lme_date_data(timer: func.TimerRequest):
+    with sqlalchemy.orm.Session(pg_engine) as session:
+        lme_exchange_obj = session.get(Exchange, "xlme")
+        if lme_exchange_obj is None:
+            raise ValueError("Unable to find LME exchange under symbol `xlme`")
+        for product in lme_exchange_obj.products:
+            lme_staticdata_utils.update_lme_product_static_data(product, session)
+            # per product update logic for health keys could go here or elsewhere
