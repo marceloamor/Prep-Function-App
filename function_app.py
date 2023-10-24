bcaf9a75750d6c9d4985010a83cd322a34927682
fwf-upetrading <frederick.fillingham@upetrading.com>
2023-10-24 10:38:08 +0100

    Add redis pubsub compute channel handling

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+from upedata.static_data import Option, Exchange
 import prep.nightly as nightly_funcs
 
 from redis.backoff import ExponentialBackoff
@@ -9,11 +10,15 @@ import redis
 
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from typing import List
 import logging
+import json
 import os
 
 
 app = func.FunctionApp()
+
+REDIS_COMPUTE_CHANNEL = os.getenv("REDIS_COMPUTE_CHANNEL", "v2:compute")
 
 redis_conn = redis.Redis(
     host=os.getenv("REDIS_HOST"),  # type: ignore
@@ -47,26 +52,31 @@ pg_engine = sqlalchemy.create_engine(sqlalchemy_pg_url, echo=False)
 # underlying redis health keys
 
 
-@app.function_name(name="rjo_sftp_update_interest_rates")
-@app.schedule(schedule="15 4/30 2-12 * * TUE-SAT", arg_name="timer")
-def check_for_new_lme_overnight_files(timer: func.TimerRequest):
+@app.function_name(name="rjo_sftp_update_inr_data")
+@app.schedule(schedule="15 4/30 21-10 * * MON-FRI", arg_name="timer")
+def update_inr_data(timer: func.TimerRequest):
     logging.info("Updating INR data")
-    nightly_funcs.update_currency_interest_curves_from_lme(
+    inr_updated = nightly_funcs.update_currency_interest_curves_from_lme(
         redis_conn, pg_engine, first_run=True
     )
+    if inr_updated:
+        send_lme_cache_update()
+        send_euronext_cache_update()
 
 
 @app.function_name(name="rjo_sftp_update_fcp_data")
-@app.schedule(schedule="15 5/30 2-12 * * TUE-SAT", arg_name="timer")
+@app.schedule(schedule="15 5/30 21-10 * * MON-FRI", arg_name="timer")
 def update_fcp_data(timer: func.TimerRequest):
     logging.info("Updating FCP data")
-    nightly_funcs.update_future_closing_prices_from_lme(
+    fcp_updated = nightly_funcs.update_future_closing_prices_from_lme(
         redis_conn, pg_engine, first_run=True
     )
+    if fcp_updated:
+        send_lme_cache_update()
 
 
 @app.function_name(name="rjo_sftp_update_clo_data")
-@app.schedule(schedule="15 6/30 2-12 * * TUE-SAT", arg_name="timer")
+@app.schedule(schedule="15 6/30 21-10 * * MON-FRI", arg_name="timer")
 def update_clo_data(timer: func.TimerRequest):
     logging.info("Updating CLO data")
     nightly_funcs.update_option_closing_prices_from_lme(
@@ -75,7 +85,7 @@ def update_clo_data(timer: func.TimerRequest):
 
 
 @app.function_name(name="rjo_sftp_update_exr_data")
-@app.schedule(schedule="15 7/30 2-12 * * TUE-SAT", arg_name="timer")
+@app.schedule(schedule="15 7/30 21-10 * * MON-FRI", arg_name="timer")
 def update_exr_data(timer: func.TimerRequest):
     logging.info("Updating EXR data")
     nightly_funcs.update_exchange_rate_curves_from_lme(redis_conn, pg_engine)
@@ -83,7 +93,7 @@ def update_exr_data(timer: func.TimerRequest):
 
 @app.function_name(name="lme_date_data_updater")
 @app.schedule(
-    schedule="32 1 2 * * MON-FRI",
+    schedule="32 1 20 * * SUN-THU",
     arg_name="timer",
     use_monitor=True,
 )
@@ -96,3 +106,70 @@ def update_lme_date_data(timer: func.TimerRequest):
         # placeholder_dt=datetime(2023, 1, 1, tzinfo=ZoneInfo("Europe/London")),
     )
     logging.info("Completed LME static data update")
+
+
+def send_static_data_update_for_product_ids(
+    channel_key: str, options_to_update: List[Option]
+):
+    """Send a list of option symbols down the given pubsub channel to force updating
+    of the attached option engine cache
+
+    :param channel_key: Pubsub channel identifier key
+    :type channel_key: str
+    :param options_to_update: List of options to have symbols sent down, only the first will
+    have the `staticdata` marker attached which causes a refresh of all cached static data
+    information.
+    :type options_to_update: List[Option]
+    """
+    pipeline = redis_conn.pipeline()
+    # i know the channel_key + ... is duplicated and done several times, but when sanity
+    # checking to make sure this separation system works patterns are used to make sure
+    # keys have this included on publish and set
+    redis_conn.publish(
+        channel_key + nightly_funcs.redis_dev_key_append,
+        json.dumps([options_to_update[0].symbol, "staticdata"]),
+    )
+    if len(options_to_update) > 1:
+        for option_obj in options_to_update[1:]:
+            pipeline.publish(
+                channel_key + nightly_funcs.redis_dev_key_append,
+                json.dumps([option_obj.symbol, "update"]),
+            )
+
+    pipeline.execute()
+    logging.info(
+        "Sent %s option symbol cache updates on channel: `%s`",
+        len(options_to_update),
+        channel_key,
+    )
+
+
+def get_options_from_exchange_symbol_static_data(
+    sqla_session: sqlalchemy.orm.Session, exchange_symbol: str
+) -> List[Option]:
+    # this sort of pattern is fine in batch jobs but for stuff running
+    # regularly during the trading day would be best packaged into a single
+    # text query
+    exchange = sqla_session.get(Exchange, exchange_symbol)
+    if exchange is None:
+        raise ValueError(f"Exchange with symbol `{exchange_symbol} was not found")
+
+    options: List[Option] = []
+    for product_obj in exchange.products:
+        options.extend(product_obj.options)
+
+    return options
+
+
+def send_lme_cache_update():
+    logging.info("Sending LME cache update command on redis")
+    with sqlalchemy.orm.Session(pg_engine) as session:
+        lme_options = get_options_from_exchange_symbol_static_data(session, "xlme")
+        send_static_data_update_for_product_ids(REDIS_COMPUTE_CHANNEL, lme_options)
+
+
+def send_euronext_cache_update():
+    logging.info("Sending XEXT cache update command on redis")
+    with sqlalchemy.orm.Session(pg_engine) as session:
+        xext_options = get_options_from_exchange_symbol_static_data(session, "xext")
+        send_static_data_update_for_product_ids(REDIS_COMPUTE_CHANNEL, xext_options)
